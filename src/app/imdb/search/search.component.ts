a78b74c346d8e6441b168e82cd404fcad9a785df
Pawel Rygiewicz <p.rygiewicz@gmail.com>
2023-12-17 18:10:56 +0100

    Use search phrase from url

--- src/app/imdb/search/search.component.ts
+++ src/app/imdb/search/search.component.ts
@@ -1,19 +1,32 @@
+import { AsyncPipe } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, AsyncPipe],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss',
 })
 export class SearchComponent {
-  @Output() onSearch = new EventEmitter<string>();
+  @Output() onSearch: Observable<string>;
+
+  constructor(private router: Router, private activeRoute: ActivatedRoute) {
+    this.onSearch = this.activeRoute.queryParamMap.pipe(
+      map((params) => {
+        const value = params.get('p');
+
+        return value || '';
+      })
+    );
+  }
 
   onSubmit(f: NgForm) {
     const phrase = String(f.value.phrase || '');
 
-    this.onSearch.emit(phrase);
+    this.router.navigate([], { queryParams: { p: phrase } });
   }
 }
