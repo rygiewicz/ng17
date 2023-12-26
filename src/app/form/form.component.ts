81cd7413abe07fed3533378b1c3c8eb7ded22b95
Pawel Rygiewicz <p.rygiewicz@gmail.com>
2023-12-26 12:38:40 +0100

    Add some validation

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,21 +1,41 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidatorFn,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss',
 })
 export class FormComponent {
   form = new FormGroup({
     text1: new FormControl(''),
+    exact1: new FormControl('', exactName('Houdini')),
     email1: new FormControl(''),
     password1: new FormControl(''),
   });
 
   onSubmit() {
-    console.log(this.form.value);
+    console.log(this.form);
   }
+}
+
+function exactName(name: string): ValidatorFn {
+  return (control: AbstractControl) => {
+    if (control.value === name) {
+      return null;
+    }
+
+    return {
+      exactName: `Name should be exactly ${name}`,
+    };
+  };
 }
